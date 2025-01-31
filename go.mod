60634c226c8abbad5801cd0c2d8b6d51254bb408
Nurali Techie <nurali.techie@gmail.com>
2025-02-01 00:24:50 +0530

    feat: call chat with stream false and true

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/go-deepseek/deepseek-demo
 
 go 1.23.4
 
-require github.com/go-deepseek/deepseek v0.0.0-20250130183020-846240cf0476
+require github.com/go-deepseek/deepseek v0.0.0-20250131184913-e968b0dc5e5a
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
 	"github.com/go-deepseek/deepseek"
+	"github.com/go-deepseek/deepseek/request"
 )
 
 const DEEPSEEK_API_KEY = `sk-123cd456c78d9be0b123de45cf6789b0` // replace with valid one
@@ -25,58 +27,65 @@ func main() {
 			panic(err)
 		}
 		inMsg := string(lineBytes)
-		fmt.Println(inMsg)
 
 		if strings.ToLower(inMsg) == "bye" || strings.ToLower(inMsg) == "exit" || strings.ToLower(inMsg) == "quit" {
 			fmt.Println("bye!!!")
 			os.Exit(0)
 		}
 
-		// req := &deepseek.DeepseekChatRequest{
-		// 	Model: "deepseek-chat",
-		// 	Messages: []*deepseek.Message{
-		// 		{
-		// 			Role:    "system",
-		// 			Content: "You are a helpful assistant.",
-		// 		},
-		// 		{
-		// 			Role:    "user",
-		// 			Content: inMsg,
-		// 		},
-		// 	},
-		// 	Stream: true,
-		// }
+		fmt.Println("stream=false")
+		callChat(client, inMsg)
+		fmt.Println()
 
-		req := &deepseek.DeepseekChatRequest{
-			Model: "deepseek-reasoner",
-			Messages: []*deepseek.Message{
-				{
-					Role:    "user",
-					Content: inMsg,
-				},
-			},
-			Stream: true,
-		}
-
-		iter, err := client.StreamChatCompletionsReasoner(req)
-		if err != nil {
-			panic(err)
-		}
-
-		// fmt.Println(iter.Choices[0].Message.ReasoningContent)
-		// fmt.Println()
-		// fmt.Println()
-		// fmt.Println(iter.Choices[0].Message.Content)
-
-		for {
-			msg := iter.Next()
-			if msg == nil {
-				break
-			}
-			fmt.Print(msg.Choices[0].Delta.ReasoningContent)
-			fmt.Print(msg.Choices[0].Delta.Content)
-		}
+		fmt.Println("stream=true")
+		streamChat(client, inMsg)
 		fmt.Println()
 	}
 
+}
+
+func callChat(client deepseek.Client, inMsg string) {
+	req := &request.ChatCompletionsRequest{
+		Model: "deepseek-chat",
+		Messages: []*request.Message{
+			{
+				Role:    "user",
+				Content: inMsg,
+			},
+		},
+		Stream: false,
+	}
+	resp, err := client.CallChatCompletionsChat(req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(resp.Choices[0].Message.Content)
+}
+
+func streamChat(client deepseek.Client, inMsg string) {
+	req := &request.ChatCompletionsRequest{
+		Model: "deepseek-chat",
+		Messages: []*request.Message{
+			{
+				Role:    "user",
+				Content: inMsg,
+			},
+		},
+		Stream: true,
+	}
+	sr, err := client.StreamChatCompletionsChat(req)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		resp, err := sr.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		fmt.Print(resp.Choices[0].Delta.Content)
+	}
 }
